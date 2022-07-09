2f5ad037f7abcac04d3bb104ace9c243883f5dd7
hvalena <hannahvalena@gmail.com>
2022-07-09 12:52:13 -0400

    started navbar and hero

---
+++ components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className="w-full h-screen text-center">
+        <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+            <div>
+                <p className="tracking-widest text-gray-600">Hello! My name is</p>
+                <h1 className="text-[#83C5BE] py-1">Hannah Valena</h1>
+                <p className="text-sm py-1">Welcome to my digital garden, where I share my growth as a <span className="font-bold">software developer</span></p>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
